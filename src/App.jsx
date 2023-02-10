3f7fa10981d3e4197cadca5a3844b0daac8bac3a
Alfredo Arroyo <alfredo7891@gmail.com>
2023-02-09 23:41:39 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="form">
+      <header>
+      <h1>Buscador de peliculas</h1>
+      <form action="" className="form">
+        <input type="text" placeholder='Buscar pelicula...' />
+        <button type="submit">Buscar</button>
+      </form>
+      </header>
+
+      <main>
+        Aqui los resultados
+      </main>
+    </div>
+  )
+}
+
+export default App
